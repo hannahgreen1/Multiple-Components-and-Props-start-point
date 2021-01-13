be998c0696a9f2af3cad43b52b37c6d62bccd0f0
hannahgreen1 <hannahgreen94@icloud.com>
2021-01-13 10:20:34 +0000

    initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <h1>My Shopping List</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      items: [
+        {name: 'Milk', isPurchased: false},
+        {name: 'Cheese', isPurchased: true},
+        {name: 'Beans', isPurchased: false},
+      ]
+    };
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
